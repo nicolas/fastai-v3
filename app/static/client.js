5826386faa8ed8398a7b8f6cdf9dc7b16b3a1116
Nicolas <nicolas@users.ai>
2019-06-18 08:36:21 +0200

    is it an air?

--- app/static/client.js
+++ app/static/client.js
@@ -21,15 +21,23 @@ function analyze() {
   el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
+  xhr.open(
+    "POST",
+    `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true
+  );
   xhr.onerror = function() {
     alert(xhr.responseText);
   };
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      if (response["result"] == "cutback") {
+        response = "It is *not* an air";
+      } else {
+        response = "It is an air";
+      }
+      el("result-label").innerHTML = `${response}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
@@ -38,4 +46,3 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
